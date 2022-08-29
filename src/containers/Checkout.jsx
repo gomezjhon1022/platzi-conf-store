e4d7431f9ff5289bafd3492c051c64819bfe1f05
Jhon Gomez <jhonjairogomez92@gmail.com>
2022-08-29 16:52:41 -0500

    Añadir maquetación y estilos del formulario de checkout

--- src/containers/Checkout.jsx
+++ src/containers/Checkout.jsx
@@ -1,8 +1,24 @@
-import React from 'react'
+import React from 'react';
+import '../styles/components/Checkout.css'
 
 const Checkout = () => {
   return (
-    <div>Checkout</div>
+    <div className="checkout">
+      <div className="checkout-content">
+        <h3>Lista de Pedidos:</h3>
+        <div className="checkout-item">
+          <div className="checkout-element">
+            <h4>ITEM name</h4>
+            <span> $10</span>
+          </div>
+          <button type="button">Eliminar</button>
+        </div>
+      </div>
+      <div className="checkout-sidebar">
+        <h3>Precio Total: $10</h3>
+        <button type="button">Continuar pedido</button>
+      </div>
+    </div>
   )
 }
 
