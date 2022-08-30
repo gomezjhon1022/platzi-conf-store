37a27dca648b84f777a300e3f1c92cf0170ef14d
Jhon Gomez <jhonjairogomez92@gmail.com>
2022-08-29 20:55:55 -0500

    Cambiar products.js para implementar useContext

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
+import AppContext from '../context/AppContext';
 import '../styles/components/Header.css';
 
 
 const Header = () => {
+  const {state: { cart }} = useContext(AppContext);
   return (
     <div className="header">
       <h1 className="header-title">
@@ -13,8 +15,9 @@ const Header = () => {
       </h1>
       <div className="header-checkout">
         <Link to="/checkout">
-          <i className="fas fa-shopping-basket fa-2x"></i>
+          <i className="fas fa-shopping-basket"></i>
         </Link>
+        {cart.length > 0 && <div className="header-alert">{cart.length}</div>}
       </div>
     </div>
   );
--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,13 +1,19 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import Product from './Product';
 import '../styles/components/Products.css';
+import AppContext from '../context/AppContext';
 
-const Products = ({products}) => {
+const Products = () => {
+  const { state:{ products }, addToCart } = useContext(AppContext);
+
+  const handleAddToCart = product => () => {
+    addToCart(product);
+  }
   return (
     <div className="products">
       <div className="products-items">
         {products.map(product => (
-          <Product key={product.id} product={product} />
+          <Product key={product.id} product={product} handleAddToCart={handleAddToCart} />
         ))}
       </div>
     </div>
